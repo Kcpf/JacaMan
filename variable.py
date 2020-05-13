6234fbaa2e1aafe9e1c1ff8e7a5f6bea334c8f4d
Vigrel <eller.vini@gmail.com>
2020-05-13 09:24:19 -0300

    mk1.2

--- variable.py
+++ variable.py
@@ -5,7 +5,7 @@ import funcoes
 RESOLUTION = (600, 600)
 #resolução só pode ser quadrada 
 #divisão tem que ser numero inteiro 
-DIVISIONS = (15, 15) # Even numbers
+DIVISIONS = (75, 75) # Even numbers
 WIDTH_SQUARE = RESOLUTION[0] // DIVISIONS[0]
 HEIGHT_SQUARE = RESOLUTION[1] // DIVISIONS[1]
 SQUARE_DIMENSIONS = (WIDTH_SQUARE, HEIGHT_SQUARE)
